b2a899b4a2b054b7424f4147124b64f075d73eef
LuanDaniel021 <luancordeiro021@gmail.com>
2026-09-23 02:52:19 -0300

    pn

--- src/pages/home/modals/dashboard/components/outlet-components/PneusView.tsx
+++ src/pages/home/modals/dashboard/components/outlet-components/PneusView.tsx
@@ -1,8 +1,15 @@
 
+import { Header, Content } from './pneus/components/user';
+
+import styles from '../../css/PneusView.module.css'
+
 function PneusView()
 {
     return (
-        <></>
+        <div className={styles['container']}>
+            <Header />
+            <Content />
+        </div>
     )
 }
 
