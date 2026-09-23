128b52741f9e2b7c2b28d1e2a866ccf894fa25aa
LuanDaniel021 <luancordeiro021@gmail.com>
2026-09-23 09:20:11 -0300

    asd

--- src/pages/home/modals/dashboard/components/outlet-components/Veiculos.tsx
+++ src/pages/home/modals/dashboard/components/outlet-components/Veiculos.tsx
@@ -3,13 +3,30 @@ import styles from '../../css/Veiculos.module.css';
 
 import Modelo3dView from "./Modelo3dView";
 
-function Header() {
-    return (
-        <header className={styles['header']}>
-            TESTE
-        </header>
-    )
+import React from 'react';
+import styles from './Header.module.css';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.plateArea}>
+        <span className={styles.eyebrow}>Placa do Veículo</span>
+        <div className={styles.plate}>
+          <div className={styles.plateHeader}>
+            BR <span>•</span> BRASIL
+          </div>
+          <input
+            type="text"
+            placeholder="ABC1D23"
+            maxLength={7}
+          />
+          <span className={styles.plateCountry}>BR</span>
+        </div>
+      </div>
+    </header>
+  );
 }
+
 
 function Render() {
     return (
